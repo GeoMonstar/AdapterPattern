affd653d13ad2faa5501e73d7708267b638d5027
Monstar <Monstar@monstarmacbook-pro.local>
2016-08-03 16:08:22 +0800

    Initial Commit

---
+++ Adapter pattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Adapter pattern
+//
+//  Created by Monstar on 16/8/3.
+//  Copyright © 2016年 Monstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Adapter pattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Adapter pattern
+//
+//  Created by Monstar on 16/8/3.
+//  Copyright © 2016年 Monstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
